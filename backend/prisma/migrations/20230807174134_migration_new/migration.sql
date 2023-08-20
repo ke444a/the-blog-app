a76a211cd24c5af7060e402b992b50a4df9f4959
Danyl Kecha <kechaofficial@gmail.com>
2023-08-20 11:44:50 +0100

    Deleted migrations

--- backend/prisma/migrations/20230807174134_migration_new/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `createdAt` on the `Comment` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Comment" DROP COLUMN "createdAt",
-ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
